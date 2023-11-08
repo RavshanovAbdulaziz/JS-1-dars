be392cea14f558bdeb479c4067e24bf135bba6a3
RavshanovAbdulaziz <abdulazizravshanov28@gmail.com>
2023-11-08 19:02:07 +0500

    JS 1-dars
    27-guruh

---
+++ Java Script 1-dars/js/main.js
@@ -0,0 +1,53 @@
+/* JSni e'lon qilish: */
+
+/*console.log("Men console oynasida korinaman!");*/
+/*document.write("Men body oynasida korinaman!");*/
+/*alert("Sayitimizga xush kelibsiz!")*/
+/*console.log(5+5*5/5+100)*/
+
+/*let x = 2;*/
+/*let y = 5;*/
+/*let z = 10;*/
+/*x = 7;*/
+/*let x = 7, y = 5, z = 10;*/
+/*document.write(x+y+z);*/
+
+/*let salom = "Assalomu aleom!";*/
+/*let rang = "#f1f1f1";*/
+/*document.write(rang);*/
+
+/* Data types in  JS */
+
+/*let x = 5;*/               /* number */
+/*let y = "JavaScript";*/    /* string */
+/*document.write(typeof y);*/
+
+/* Math in JS */
+/*let x = 5;*/
+/*x+=22; /!* x = 5 + 1*!/*/
+/*document.write(x);*/
+
+/*let x = 0;*/
+
+/*x+=1*/      /*x = 5 + 1*/
+/*x-=1*/      /*x = 5 - 1*/
+/*x*=1*/      /*x = 5 * 1*/
+/*x/=1*/      /*x = 5 / 1*/
+/*let qoldiq = x%2;*/
+/*let daraja = x**5;*/
+/*x++;*/
+/*x++;*/
+/*x++;*/
+/*x++;*/
+/*x++; /!* block *!/*/
+
+/*document.write(x);*/
+
+
+
+
+
+
+
+
+
